c5a1875fd12b59f5ba267faaa85b1a4f5850cf8a
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-04-25 18:20:16 +0200

    extend project structure

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<?php $title = 'File Share - Home'; ?>
+<?php require_once("parts/header.php"); ?>
 
-echo "Hello.";
+<body>
+    <p>Hello!</p>
+    <?php require_once("parts/bootstrap_js.php"); ?>
+</body>
 
-?>
+</html>
