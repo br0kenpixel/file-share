f46a21751f3aaa6463bb1df248356c3d985772a5
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-05-03 11:33:46 +0200

    add basic administration tools

--- private/account.php
+++ private/account.php
@@ -16,6 +16,10 @@ $display_email = $dbClient->get_user_email($display_id);
 $user_not_found = false;
 
 if (isset($_GET["id"])) {
+    if (empty($_GET["id"])) {
+        header("Location: account.php");
+    }
+
     if (!$_SESSION["is_admin"]) {
         header("Location: account.php");
     } else {
@@ -100,13 +104,34 @@ if (isset($_GET["id"])) {
                     class="btn btn-danger">Delete my account</button></a>
             <?php if ($_SESSION["is_admin"]) {
                 ?>
-                <button type="button" class="btn btn-warning">Manage another account</button>
+                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#accountModal">Manage
+                    another account</button>
                 <?php
             } ?>
         </div>
         <?php
     }
     ?>
+
+    <div class="modal fade" id="accountModal" tabindex="-1" aria-labelledby="accountModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="accountModalLabel">Manage another account</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form method="get" action="account.php">
+                        <div class="mb-3">
+                            <label for="accountId" class="form-label">ID</label>
+                            <input type="number" class="form-control" id="accountId" name="id">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
--- private/index.php
+++ private/index.php
@@ -13,6 +13,7 @@ use fileshare\components\Formatter;
 
 $dbClient = new DatabaseClient();
 $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
+$file_count = $dbClient->get_user_file_count($_SESSION["id"]);
 ?>
 
 <!DOCTYPE html>
@@ -44,76 +45,135 @@ $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_
 
     <br />
 
-    <div class="container" style="display: none;">
-        <p>You currently don't have any files.</p>
-    </div>
-
-    <div class="container">
-        <table class="table table-hover table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Size</th>
-                    <th scope="col">Kind</th>
-                    <th scope="col">Uploaded on</th>
-                    <th scope="col">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                foreach ($dbClient->get_user_files($_SESSION["id"]) as $key => $value) {
-                    ?>
-                    <tr>
-                        <th scope="row">
-                            <?php echo $value["name"]; ?>
-                        </th>
-                        <td>
-                            <?php echo Formatter::pretty_size(FileSize::get_size($_SESSION["username"], $value["name"])); ?>
-                        </td>
-                        <td>
-                            <?php echo Formatter::get_file_kind($value["name"]); ?>
-                        </td>
-                        <td>
-                            <?php echo $value["upload_time"]; ?>
-                        </td>
-                        <td class="vallign">
-                            <a href="<?php echo "/download.php?file=" . $value["id"]; ?>"><button type="button"
-                                    class="btn btn-primary btn-sm">&#11015;&#65039; Download</button></a>
-                            <a href="<?php echo $root . "share.php?file=" . $value["id"] ?>"><button id="copy-btn"
-                                    type="button" class="btn btn-success btn-sm">&#128206; Share link</button></a>
-                            <a href="<?php echo "/remove.php?file=" . $value["id"]; ?>"><button type="button"
-                                    class="btn btn-danger btn-sm">&#10060; Delete</button></a>
-
-                        </td>
-                    </tr>
-                    <?php
-                }
-                ?>
-            </tbody>
-        </table>
-
-        <?php
-        $cap = $dbClient->get_user_limit($_SESSION["id"]);
-        $usage = FileSize::get_user_usage($_SESSION["username"]);
-        $usage_percentage = FileSize::calc_usage_percentage($cap, $usage);
+    <?php if ($file_count === 0) {
         ?>
-
-        <p>Files count:
-            <?php echo $dbClient->get_user_file_count($_SESSION["id"]) ?>
-        </p>
-        <p>Storage usage:
-            <?php echo Formatter::pretty_size($usage); ?>
-        </p>
-
-        <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0"
-            aria-valuemax="100">
-            <div class="progress-bar" style="width: <?php echo $usage_percentage; ?>%">
-                <?php echo $usage_percentage; ?>%
-            </div>
+        <div class="container">
+            <p>You currently don't have any files. Maybe upload some?</p>
         </div>
-        <p>Storage limit:
-            <?php echo Formatter::pretty_size($cap); ?>
-        </p>
+        <?php
+    } else { ?>
+        <?php if ($_SESSION["is_admin"]) {
+            ?>
+            <div class="container">
+                <hr>
+                <p>Administration:</p>
+
+                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#accountModal">Lookup
+                    account</button>
+                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#fileModal">Lookup
+                    file</button>
+                <hr>
+            </div>
+
+            <div class="modal fade" id="accountModal" tabindex="-1" aria-labelledby="accountModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="accountModalLabel">Manage another account</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="get" action="account.php">
+                                <div class="mb-3">
+                                    <label for="accountId" class="form-label">ID</label>
+                                    <input type="number" class="form-control" id="accountId" name="id">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="fileModal" tabindex="-1" aria-labelledby="fileModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="fileModalLabel">Manage share</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <form method="get" action="/share.php">
+                                <div class="mb-3">
+                                    <label for="fileId" class="form-label">File ID</label>
+                                    <input type="number" class="form-control" id="fileId" name="file">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <?php
+        } ?>
+
+        <div class="container">
+            <table class="table table-hover table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Size</th>
+                        <th scope="col">Kind</th>
+                        <th scope="col">Uploaded on</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($dbClient->get_user_files($_SESSION["id"]) as $key => $value) {
+                        ?>
+                        <tr>
+                            <th scope="row">
+                                <?php echo $value["name"]; ?>
+                            </th>
+                            <td>
+                                <?php echo Formatter::pretty_size(FileSize::get_size($_SESSION["username"], $value["name"])); ?>
+                            </td>
+                            <td>
+                                <?php echo Formatter::get_file_kind($value["name"]); ?>
+                            </td>
+                            <td>
+                                <?php echo $value["upload_time"]; ?>
+                            </td>
+                            <td class="vallign">
+                                <a href="<?php echo "/download.php?file=" . $value["id"]; ?>"><button type="button"
+                                        class="btn btn-primary btn-sm">&#11015;&#65039; Download</button></a>
+                                <a href="<?php echo $root . "share.php?file=" . $value["id"] ?>"><button id="copy-btn"
+                                        type="button" class="btn btn-success btn-sm">&#128206; Share link</button></a>
+                                <a href="<?php echo "/remove.php?file=" . $value["id"]; ?>"><button type="button"
+                                        class="btn btn-danger btn-sm">&#10060; Delete</button></a>
+
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                    ?>
+                </tbody>
+            </table>
+            <?php
+            $cap = $dbClient->get_user_limit($_SESSION["id"]);
+            $usage = FileSize::get_user_usage($_SESSION["username"]);
+            $usage_percentage = FileSize::calc_usage_percentage($cap, $usage);
+            ?>
+
+            <p>Files count:
+                <?php echo $file_count; ?>
+            </p>
+            <p>Storage usage:
+                <?php echo Formatter::pretty_size($usage); ?>
+            </p>
+
+            <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="25" aria-valuemin="0"
+                aria-valuemax="100">
+                <div class="progress-bar" style="width: <?php echo $usage_percentage; ?>%">
+                    <?php echo $usage_percentage; ?>%
+                </div>
+            </div>
+            <p>Storage limit:
+                <?php echo Formatter::pretty_size($cap); ?>
+            </p>
+        <?php } ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
